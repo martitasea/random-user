d40adea2711e8b0b3ea1fccd31c63fd5a4345dfa
Marta Pulido Asín <marta.pulido.asin@gmail.com>
2021-11-09 16:32:14 +0100

    UserCard (#1)

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,7 @@
 import {createTheme} from '@material-ui/core/styles';
 
-const primaryColor = '#973572';
+const primaryColor = '#ff5964';
+const secondaryColor = '#38618c';
 
 const theme = (type) => createTheme({
   palette: {
@@ -10,12 +11,12 @@ const theme = (type) => createTheme({
       contrastText: '#fff',
     },
     secondary: {
-      main: '#107540',
+      main: secondaryColor,
     },
     text: {
       primary: '#000000',
       secondary: '#746F81',
-    }
+    },
   },
   '@global': {
     '*::-webkit-scrollbar': {
